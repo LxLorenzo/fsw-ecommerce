11915d69193ddd771243af0bf88ac321aff67055
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-11-13 14:34:04 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
-'use client'
-
 import Image from 'next/image'
+import Categories from './components/categories'
 
 export default function Home() {
   return (
@@ -13,6 +12,8 @@ export default function Home() {
         sizes="100vw"
         alt="Até 55% de desconto esse mês"
       />
+      <div className="mt-8"></div>
+      <Categories />
     </div>
   )
 }
